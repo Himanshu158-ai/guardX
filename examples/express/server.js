015f4368ba405344a74427f1b8932cc87823afbf
Himanshu Singh <hs5317610@gmail.com>
2026-07-01 10:15:53 +0530

    feat: add express server example to demonstrate guardx middleware implementation

--- examples/express/server.js
+++ examples/express/server.js
@@ -1,22 +1,32 @@
 import express from "express";
-import guardx from "../../src/middlewares/guardx.js";
+import guardx from "../../src/index.js";
 
 const app = express();
 
 app.use(
     guardx({
-        limit: 5,
-        windowMs: 10000
+        limit: 2,
+        windowMs: 60000,
+        standardHeaders: true,
+
+        handler(req, res, info) {
+            return res.status(429).json({
+                success: false,
+                message: "Too many requests",
+                retryAfter: info.retryAfter,
+                remaining: info.remaining,
+                limit: info.limit
+            });
+        }
     })
 );
 
 app.get("/", (req, res) => {
     res.json({
-        success: true,
         message: "Hello GuardX 🚀"
     });
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Running...");
 });
